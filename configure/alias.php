23089c5900afbca86bd2d535f59fe6d031368449
ussiel <ussiel@gmail.com>
2023-11-08 22:38:56 -0600

    correccion id

--- configure/alias.php
+++ configure/alias.php
@@ -2,5 +2,6 @@
 return array
 (
     "admin/galeria/uploadify.php" => PROJECT_DIR."/modulos/galeria/controller/admin/galeria/uploadify.php",
+    "mi-cuenta/galeria/uploadify.php" => PROJECT_DIR."/modulos/galeria/controller/mi-cuenta/galeria/uploadify.php",
 );
 ?>
