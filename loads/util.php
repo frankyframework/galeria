c9d2748c5812f213723ea908bdfef41d459e9b6a
ussiel <ussiel@gmail.com>
2023-11-08 22:53:45 -0600

    correccion funcion

--- loads/util.php
+++ loads/util.php
@@ -44,7 +44,7 @@ function getMiFotoGaleria($id,$album,$foto,$descripcion,$fecha,$admin,$count_img
     
     $html = "";
     $html .= "<div class='w-xxxx-2 w-xxx-3 w-xx-4 w-x-6 align_center img_foto_clientes foto_$id' id='foto_$id'>"
-            .($MyAccessList->MeDasChancePasar("administrar_mi_galeria") && $admin == 1 ? "<div><a href=\"javascript:void(0);\" onclick=\"eliminarFotoMiGaleria($id)\"><i class='icon icon-r-eliminar'></i></a></div>" : "")
+            .($MyAccessList->MeDasChancePasar("administrar_mi_galeria") && $admin == 1 ? "<div><a href=\"javascript:void(0);\" onclick=\"eliminarMiFotoGaleria($id)\"><i class='icon icon-r-eliminar'></i></a></div>" : "")
             .($MyAccessList->MeDasChancePasar("administrar_mi_galeria") && $admin == 1 ? "<div><a href=\"javascript:void(0);\" onclick=\"promptEditarMiFoto('Nombre de la foto:','Editar foto',$id )\"><i class='icon icon-editar'></i></a></div>": "")
             . "<div>".($admin == 0 ? "<a href=\"#gid=1&amp;pid=$count_img\" rel='galeria' title=\"$descripcion\">" : "").  makeHTMLImg(imageResize($MyConfigure->getUploadDir()."/galeria/$album/$foto",220,220,true), "100%", "", $descripcion).($admin == 0 ? "</a>" : "")."</div>"
             . "<p class='txt_gal_description label_descripcion_foto_$id'>$descripcion</p>" 
