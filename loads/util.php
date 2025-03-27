92ca0742ae06c7885616ba880a84f0f626b9dc85
ussiel <ussiel@gmail.com>
2025-03-27 16:53:52 -0600

    clase galeria foto

--- loads/util.php
+++ loads/util.php
@@ -23,7 +23,7 @@ function getFotoGaleria($id,$album,$foto,$descripcion,$fecha,$admin,$count_img)
 
 
 
-function getAlbumGaleria($id,$nombre,$fecha,$admin=0)
+function getAlbumGaleria($id,$nombre,$fecha,$nombreUsuario,$admin=0)
 {
     global $MyAccessList;
     global $MyConfigure;
@@ -32,7 +32,7 @@ function getAlbumGaleria($id,$nombre,$fecha,$admin=0)
     $html .= "<div class='w-xxxx-2 w-xxx-3 w-xx-4 w-x-6 align_center img_foto_clientes album_galeria_content album_$id' id='album_$id'>"
             .($MyAccessList->MeDasChancePasar("administrar_galeria") && $admin == 1 ? "<div><a href=\"javascript:void(0);\" onclick=\"eliminarAlbumGaleria('$id')\"><i class='icon icon-r-eliminar'></i></a></div>" : "")
             .($MyAccessList->MeDasChancePasar("administrar_galeria") && $admin == 1 ? "<div><a href=\"javascript:void(0);\" onclick=\"promptEditarAlbum('Nombre del album:','Editar album',$id )\"><i class='icon icon-editar'></i></a></div>": "")
-            . "<div><h3><a href=\"".($admin == 1 ? $MyRequest->link(ADMIN_FOTOS_GALERIA."?album=".$id) : $MyRequest->url(GALERIA_DETALLE,array("album" => $id)))."\"  title=\"$nombre\" data-name=\"$id\">$nombre</a></h3></div>"
+            . "<div><h3><a href=\"".($admin == 1 ? $MyRequest->link(ADMIN_FOTOS_GALERIA."?album=".$id) : $MyRequest->url(GALERIA_DETALLE,array("album" => $id)))."\"  title=\"$nombre\" data-name=\"$id\">$nombre ".(!empty($nombreUsuario) ? "[".$nombreUsuario."]" : "")."</a></h3></div>"
             . "</div>";
     return $html;
 }
